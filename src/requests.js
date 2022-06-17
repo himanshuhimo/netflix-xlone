8bd3c9b425372ac8ec375bb3146ec280e17ca59d
himanshuhimo <himanshuhdkumar@gmail.com>
2022-06-17 19:58:06 +0530

    First Commit

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+// const API_KEY = "6cd68dfbcd9488554e1079326a62bb63";
+const API_KEY = "078cd085080b113df8e6e83a8156160d";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
